1d51a448cf8d510b1f6d1387b0ce4201dda3cc70
Ramprit Sahani <21260321+Ramprits@users.noreply.github.com>
2021-04-29 13:43:58 +0530

    Initial commit

---
+++ src/routes/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Project from "../views/Project.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/projects",
+    name: "Projects",
+    component: Project
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register
+  }
+];
+
+const router = createRouter({
+  history: createWebHashHistory(window.process.env.BASE_URL),
+  routes
+});
+
+export default router;
---
+++ src/views/Project.vue
@@ -0,0 +1,120 @@
+<template>
+  <vue-title title="Project Page" />
+  <section class="section">
+    <div class="container">
+      <div class="columns is-multiline mb-6">
+        <div class="column is-5">
+          <h2 class="title is-spaced">
+            <span>Make your</span>
+            <span class="has-text-primary">life easier</span>
+            <span>for marketing sales and customer support</span>
+          </h2>
+        </div>
+        <div class="column is-7">
+          <p class="subtitle">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus
+            eget justo et iaculis. Quisque vitae nulla malesuada, auctor arcu
+            vitae, luctus nisi. Sed elementum vitae ligula id imperdiet.
+          </p>
+        </div>
+      </div>
+      <div class="columns is-multiline">
+        <div class="column is-6-tablet is-3-desktop">
+          <div class="box py-6 has-text-centered">
+            <svg
+              class="mb-4 mx-auto image is-32x32 has-text-primary"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z"
+              ></path>
+            </svg>
+            <h3 class="title is-size-5 is-spaced mb-3">Category Example</h3>
+            <p class="subtitle">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+        <div class="column is-6-tablet is-3-desktop">
+          <div class="box py-6 has-text-centered">
+            <svg
+              class="mb-4 mx-auto image is-32x32 has-text-primary"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2"
+              ></path>
+            </svg>
+            <h3 class="title is-size-5 is-spaced mb-3">Category Example</h3>
+            <p class="subtitle">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+        <div class="column is-6-tablet is-3-desktop">
+          <div class="box py-6 has-text-centered">
+            <svg
+              class="mb-4 mx-auto image is-32x32 has-text-primary"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M13 10V3L4 14h7v7l9-11h-7z"
+              ></path>
+            </svg>
+            <h3 class="title is-size-5 is-spaced mb-3">Category Example</h3>
+            <p class="subtitle">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+        <div class="column is-6-tablet is-3-desktop">
+          <div class="box py-6 has-text-centered">
+            <svg
+              class="mb-4 mx-auto image is-32x32 has-text-primary"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
+              ></path>
+            </svg>
+            <h3 class="title is-size-5 is-spaced mb-3">Category Example</h3>
+            <p class="subtitle">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
